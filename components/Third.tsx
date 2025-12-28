77dd50fc20b96bc78763e8e92cd844f2ae11f565
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-28 14:48:21 +0400

    Third is almost finished, only problem remains is to understand why does not the center work on modal

--- components/Third.tsx
+++ components/Third.tsx
@@ -1,21 +1,38 @@
 import {
-  View,
+  Modal,
   Text,
   ImageBackground,
   StyleSheet,
   Pressable,
+  Image,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 const Third = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+  const toggleOn = () => setToggle(true);
+  const toggleOff = () => setToggle(false);
   return (
     <ImageBackground
       source={require("../assets/nature.jpg")}
       style={[styles.natureImage, styles.center]}
     >
       <Pressable style={[styles.btn, styles.center]}>
-        <Text style={styles.btnText}>See details</Text>
+        <Text style={styles.btnText} onPress={toggleOn}>
+          See details
+        </Text>
       </Pressable>
+      {toggle && (
+        <Modal animationType="fade" style={[styles.modal, styles.center]}>
+          <Text>This is a secret information</Text>
+          <Pressable onPress={toggleOff} style={styles.exitBtn}>
+            <Image
+              source={require("../assets/exit.png")}
+              style={styles.exitIcon}
+            />
+          </Pressable>
+        </Modal>
+      )}
     </ImageBackground>
   );
 };
@@ -39,5 +56,17 @@ const styles = StyleSheet.create({
   center: {
     justifyContent: "center",
     alignItems: "center",
+  },
+  modal: {
+    position: "relative",
+  },
+  exitBtn: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+  },
+  exitIcon: {
+    width: 30,
+    height: 30,
   },
 });
