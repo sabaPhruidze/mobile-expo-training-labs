59c28e8471f3089b598292062abba378dcf399c5
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-28 14:28:14 +0400

    building third

--- components/Third.tsx
+++ components/Third.tsx
@@ -1,12 +1,43 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  Pressable,
+} from "react-native";
 import React from "react";
 
 const Third = () => {
   return (
-    <View>
-      <Text>Third</Text>
-    </View>
+    <ImageBackground
+      source={require("../assets/nature.jpg")}
+      style={[styles.natureImage, styles.center]}
+    >
+      <Pressable style={[styles.btn, styles.center]}>
+        <Text style={styles.btnText}>See details</Text>
+      </Pressable>
+    </ImageBackground>
   );
 };
 
 export default Third;
+
+const styles = StyleSheet.create({
+  natureImage: {
+    flex: 1,
+  },
+  btn: {
+    width: 150,
+    height: 40,
+    borderRadius: 60,
+    backgroundColor: "#ffffffff",
+  },
+  btnText: {
+    fontSize: 18,
+    color: "#000",
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
