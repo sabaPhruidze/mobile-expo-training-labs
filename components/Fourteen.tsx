ad01fb741183c93244746a262899c1c766084471
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-21 14:19:44 +0400

    problem was that I was trying to make ionicons animated, but than tried using animed view as a wrapper for ionIcons and done it works , also asddded loop and sequence which was new to me and I am more glad that I learned how to install nativeWind so from now on I will use this if something different is not necessary

--- components/Fourteen.tsx
+++ components/Fourteen.tsx
@@ -1,28 +1,45 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, Pressable, Animated } from "react-native";
+import React, { useRef, useEffect } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../Main";
 import { Ionicons } from "@expo/vector-icons";
+
 type Props = NativeStackScreenProps<RootStackParamList, "Fourteen">;
 
 const Fourteen = ({ navigation }: Props) => {
+  const scaleRef = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(scaleRef, {
+          toValue: 1.5,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scaleRef, {
+          toValue: 1,
+          duration: 500,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+  }, []);
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Ionicons name="heart" size={100} color="yellow" />
+    <View className="flex-1 justify-center items-center bg-gray-300">
+      <Animated.View style={{ transform: [{ scale: scaleRef }] }}>
+        <Ionicons name="heart" size={100} color="red" />
+      </Animated.View>
+
       <Pressable
-        className="w-60 h-[50px] bg-white rounded-lg mt-4 items-center justify-center"
+        className="mt-10 w-[200px] h-[50px] bg-black flex justify-center rounded-xl items-center"
         onPress={() => navigation.navigate("Fifteen")}
       >
-        <Text style={styles.btnText}>Next step</Text>
+        <Text className="text-xl text-white font-extrabold">Next step</Text>
       </Pressable>
     </View>
   );
 };
 
 export default Fourteen;
-
-const styles = StyleSheet.create({
-  btnText: {
-    fontSize: 18,
-  },
-});
