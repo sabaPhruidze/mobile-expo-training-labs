22d1d04a0216999a56abcb614aeecab5d45a6572
Saba <sabaphruidze01@gmail.com>
2026-01-23 12:01:04 +0400

    This is the first time using scheduleNotification and understanding how to use it, but as I received information I can not test it without creating app, so First I will correct the button visual and that try it

--- components/Fifteen.tsx
+++ components/Fifteen.tsx
@@ -1,19 +1,48 @@
 import { View, Text,Pressable } from "react-native";
-import React from "react";
+import React,{useEffect} from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../Main";
 type Props = NativeStackScreenProps<RootStackParamList, "Fifteen">;
+import * as Notifications from 'expo-notifications'
+
+import { SafeAreaView } from "react-native-safe-area-context";
+useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+        shouldShowBanner: true,
+        shouldShowList: true,
+      }),
+    });
+  }, []);
+const scheduleNotificationAsync = async() => {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title:'Hi',
+      body:'this is a local message'
+    },
+    trigger: {
+  type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+  seconds: 2,
+    }
+  })
+}
 const Fifteen = ({ navigation }: Props) => {
   return (
-    <View>
+    <SafeAreaView className="flex-1">
       <Text>Fifteen</Text>
+      <Pressable onPress={scheduleNotificationAsync}>
+        <Text>Ring the bell</Text>
+      </Pressable>
       <Pressable
               className="mt-10 w-[200px] h-[50px] bg-black flex justify-center rounded-xl items-center"
-              onPress={() => navigation.navigate("Fifteen")}
+              onPress={() => navigation.navigate("Sixteen")}
             >
               <Text className="text-xl text-white font-extrabold">Next step</Text>
             </Pressable>
-    </View>
+    </SafeAreaView>
   );
 };
 
