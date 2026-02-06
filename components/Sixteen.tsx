1664049ad1e9fb5bc96e59689b210a0e76b9c79a
Saba <sabaphruidze01@gmail.com>
2026-02-06 11:35:41 +0400

    made a desition to move on since expo notifications takes lot's of time and there is no avaliable and concrete way as I understand so I will learn other parts of it and then get back here

--- components/Sixteen.tsx
+++ components/Sixteen.tsx
@@ -1,5 +1,5 @@
-import { View, Text,Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable } from "react-native";
+import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../Main";
 type Props = NativeStackScreenProps<RootStackParamList, "Sixteen">;
@@ -8,13 +8,13 @@ const Sixteen = ({ navigation }: Props) => {
     <View>
       <Text>Sixteen</Text>
       <Pressable
-              className="mt-10 w-[200px] h-[50px] bg-black flex justify-center rounded-xl items-center"
-              onPress={() => navigation.navigate("SevenTeen")}
-            >
-              <Text className="text-xl text-white font-extrabold">Next step</Text>
-            </Pressable>
+        className="mt-10 w-[200px] h-[50px] bg-black flex justify-center rounded-xl items-center"
+        onPress={() => navigation.navigate("SevenTeen")}
+      >
+        <Text className="text-xl text-white font-extrabold">Next step</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default Sixteen
+export default Sixteen;
