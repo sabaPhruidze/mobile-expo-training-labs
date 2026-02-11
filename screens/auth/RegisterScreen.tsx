b725f8d0aab0e2b943815390aaaf65cd91603f89
Saba <sabaphruidze01@gmail.com>
2026-02-11 11:37:38 +0400

    Build schema with name, email and password(which will check if confirmPassword has same value)

--- screens/auth/RegisterScreen.tsx
+++ screens/auth/RegisterScreen.tsx
@@ -1,5 +1,24 @@
 import { View, Text } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+
+const registerSchema = z
+  .object({
+    name: z.string().min(3, { message: "Write at least 3 letter" }),
+    email: z.email("Wrong email format"),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 letter" }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password must be at least 6 letter" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 const RegisterScreen = () => {
   return (
