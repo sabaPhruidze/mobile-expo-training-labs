01cb46aa5b54bd5e03aa5757d6c3707e6209e8a2
Saba <sabaphruidze01@gmail.com>
2026-02-13 10:46:23 +0400

    Added confirm password and used.refine for logic of password match the confirm password or not will be held in advance before moving data to firebase

--- features/auth/schema.ts
+++ features/auth/schema.ts
@@ -1,9 +1,15 @@
 import * as z from "zod";
 
-export const loginSchema = z.object({
-  fullName: z.string().min(3, "სახელი უნდა იყოს მინიმუმ 3 სიმბოლო"),
-  email: z.email("არასწორი მეილის ფორმატი"),
-  password: z.string().min(6, "პაროლი უნდა იყოს მინიმუმ 6 სიმბოლო"),
-});
+export const loginSchema = z
+  .object({
+    fullName: z.string().min(3, "სახელი უნდა იყოს მინიმუმ 3 სიმბოლო"),
+    email: z.email("არასწორი მეილის ფორმატი"),
+    password: z.string().min(6, "პაროლი უნდა იყოს მინიმუმ 6 სიმბოლო"),
+    confirmPassword: z.string().min(6, "Repeat password"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords does not match",
+    path: ["confirmPassword"],
+  });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
