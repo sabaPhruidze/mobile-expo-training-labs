912cf932ad5a91945f117be4aa699d6fd5322ae3
Saba <sabaphruidze01@gmail.com>
2026-02-16 20:20:28 +0400

    created(authSchema): now added zod but it is same as frontend so no much difference between it and also added table relevant needed on postgreSQL

---
+++ backend/src/validation/authSchemas.js
@@ -0,0 +1,15 @@
+const { z } = require("zod");
+
+export const registerSchema = z
+  .object({
+    fullName: z.string().min(3, "Name must contain at least 3 symbol"),
+    email: z.email("Wrong email format"),
+    password: z.string().min(6, "Password must be at least 6 symbol"),
+    confirmPassword: z.string().min(6, "Repeat password"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords does not match",
+    path: ["confirmPassword"],
+  });
+
+module.exports = { registerSchema };
