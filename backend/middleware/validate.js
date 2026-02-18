e2265ce71206955015909d6bea06b557958b5cd3
Saba <sabaphruidze01@gmail.com>
2026-02-18 21:00:42 +0400

    create(validate): added validator ,this way if error it will show one or as many as errors happened in array of objects, so that is why writn in this way, and safeParse gives us an object with success or fail case and than I filtered 2 ways, now I have to use it in controller

---
+++ backend/middleware/validate.js
@@ -0,0 +1,12 @@
+const validate = (schema) => (req, res, next) => {
+  const result = schema.safeParse(req.body);
+  if (!result) {
+    const errors = result.error.issues.map((issue) => ({
+      field: issue.path.join("."),
+      message: issue.message,
+    }));
+    return res.status(400).json({ message: "Validation error", errors });
+  }
+  req.validated = result.data;
+  next();
+};
