23cc9cc3d4bdb9ac381c9188db8596a17f2c0a13
Saba <sabaphruidze01@gmail.com>
2026-02-18 21:44:01 +0400

    update(authController): password_hash wrote incorrectly and corrected

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,12 +4,12 @@ const { findUserByEmail, createUser } = require("../models/userModel");
 
 const register = async (req, res) => {
   try {
-    const { fullname, email, password } = req.validated; // added by this in validate.js
+    const { fullName, email, password } = req.validated; // added by this in validate.js
     const existing = await findUserByEmail(email);
     if (existing)
       return res.status(409).json({ message: "Email already exist" });
     const password_hash = await bcrypt.hash(password, 10); //writing salt is not neccessary, this is a shorter way
-    const user = await createUser({ fullName, email, passwordHash });
+    const user = await createUser({ fullName, email, password_hash });
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
