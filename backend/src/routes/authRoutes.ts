75d6dbe932f9013e45faa786fcf122569c4fe8a9
Saba <sabaphruidze01@gmail.com>
2026-02-20 21:27:25 +0400

    update(authRoutes):moving to the next to turn next thing also typescript part

---
+++ backend/src/routes/authRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import validate from "../middleware/validate";
+import { registerSchema } from "../validation/authSchemas";
+import { register } from "../controllers/authController";
+
+const router = Router();
+router.post("/register", validate(registerSchema), register);
+
+export default router;
