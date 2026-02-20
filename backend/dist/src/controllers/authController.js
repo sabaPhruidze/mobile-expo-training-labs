2b8844a0ce25612fcd9891a7e3b2d52e997ac4e9
Saba <sabaphruidze01@gmail.com>
2026-02-20 21:05:40 +0400

    adjusted the code to work on typescript in the backend as well

---
+++ backend/dist/src/controllers/authController.js
@@ -0,0 +1,24 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { findUserByEmail, createUser } = require("../models/userModel");
+const register = async (req, res) => {
+    try {
+        const { fullName, email, password } = req.validated; // added by this in validate.js
+        const existing = await findUserByEmail(email);
+        if (existing)
+            return res.status(409).json({ message: "Email already exist" });
+        const password_hash = await bcrypt.hash(password, 10); //writing salt is not neccessary, this is a shorter way
+        const user = await createUser({ fullName, email, password_hash });
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+        return res.status(201).json({ message: "Registered", user, token });
+    }
+    catch (error) {
+        console.error("REGISTER ERROR:", error);
+        return res
+            .status(500)
+            .json({ message: "Server error", error: error.message });
+    }
+};
+module.exports = { register };
