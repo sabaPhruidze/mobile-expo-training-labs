dd8eb3e80860dd049705a6c366662dfeba30b366
Saba <sabaphruidze01@gmail.com>
2026-03-06 18:59:24 +0400

    fix(LoginScreen): forgot to write rnfe instead of rface :D

--- frontend/screens/auth/LoginScreen.tsx
+++ frontend/screens/auth/LoginScreen.tsx
@@ -1,7 +1,12 @@
+import { View, Text } from "react-native";
 import React from "react";
 
-function LoginScreen() {
-  return <div>LoginScreen</div>;
-}
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>LoginScreen</Text>
+    </View>
+  );
+};
 
 export default LoginScreen;
