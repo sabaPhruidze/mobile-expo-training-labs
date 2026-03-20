1a7f79ad3ddc01486142b3169de707ee757a97d7
Saba <sabaphruidze01@gmail.com>
2026-03-20 21:03:33 +0400

    feat(HobbyListScreen): addded list in Flatlist and now will correct it's design

--- frontend/screens/HobbyListScreen.tsx
+++ frontend/screens/HobbyListScreen.tsx
@@ -13,6 +13,7 @@ export const hobbiesData = [
   { id: 10, title: "Chess" },
   { id: 11, title: "Watching movies" },
   { id: 12, title: "Gaming" },
+  { id: 13, title: "reading" },
 ] as const;
 
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -22,12 +23,17 @@ type Props = NativeStackScreenProps<RootStackParamList, "HobbyListScreen">;
 const HobbyListScreen = ({ navigation }: Props) => {
   return (
     <View className="flex-1 bg-slate-950 px-4 pt-6">
-      <View className="rounded-3xl border border-slate-800 bg-slate-900 p-5">
-        <Text className="text-2xl font-bold text-white">My hobbies</Text>
-        <Text className="mt-2 text-sm text-slate-400">
-          Hobbies screen placeholder
-        </Text>
-      </View>
+      <FlatList
+        data={hobbiesData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View className="mb-3 rounded-2xl border-slate-800 bg-slate-900 p-4">
+            <Text className="text-base font-medium text-white">
+              {item.title}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
