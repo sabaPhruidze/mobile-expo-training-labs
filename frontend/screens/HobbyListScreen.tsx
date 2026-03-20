b9e01031be2967387246925c840eed3032fb503f
Saba <sabaphruidze01@gmail.com>
2026-03-20 20:47:39 +0400

    now Imporving fourth page. it's new name is HobbyList

---
+++ frontend/screens/HobbyListScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HobbyListScreen = () => {
+  return (
+    <View>
+      <Text>HobbyListScreen</Text>
+    </View>
+  );
+};
+
+export default HobbyListScreen;
