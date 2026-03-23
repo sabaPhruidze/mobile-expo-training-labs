f42f5d0a02cc59e013b095a574e014fcc28f3f2b
Saba <sabaphruidze01@gmail.com>
2026-03-23 18:24:48 +0400

    feat(RestaurantMenuScreen): starting updating fifth page and writing in correct place

---
+++ frontend/screens/RestaurantMenuScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RestaurantMenuScreen = () => {
+  return (
+    <View>
+      <Text>RestaurantMenuScreen</Text>
+    </View>
+  );
+};
+
+export default RestaurantMenuScreen;
