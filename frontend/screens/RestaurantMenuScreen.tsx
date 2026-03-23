664ac6e92d504f32b15614a3447b58835fc653da
Saba <sabaphruidze01@gmail.com>
2026-03-23 18:37:36 +0400

    feat(restaurantmenuscreen): update

--- frontend/screens/RestaurantMenuScreen.tsx
+++ frontend/screens/RestaurantMenuScreen.tsx
@@ -5,8 +5,13 @@ import { RootStackParamList } from "../Main";
 type Props = NativeStackScreenProps<RootStackParamList, "RestaurantMenuScreen">;
 const RestaurantMenuScreen = ({ navigation }: Props) => {
   return (
-    <View>
-      <Text>RestaurantMenuScreen</Text>
+    <View className="flex-1 bg-[#0B1220] items-center justify-center px-4">
+      <View className="w-full max-w-[360px] rounded-3xl border border-white/20 bg-white/10 p-5">
+        <Text className="text-white text-2xl font-bold text-center">
+          RestaurantMenuScreen
+        </Text>
+        <Text className="mt-2 text-center text-white/70">category of menu</Text>
+      </View>
     </View>
   );
 };
