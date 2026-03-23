44e02eba8733427cab54de360a2c05fb5e179a91
Saba <sabaphruidze01@gmail.com>
2026-03-23 19:00:08 +0400

    feat(restaurantmenuscreen):added button for moving next page and i will start sixth correction , maybe not finish today but starting would be better

--- frontend/screens/RestaurantMenuScreen.tsx
+++ frontend/screens/RestaurantMenuScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, SectionList, Pressable } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../Main";
 
@@ -77,13 +77,34 @@ const menuSections: MenuSection[] = [
 ];
 const RestaurantMenuScreen = ({ navigation }: Props) => {
   return (
-    <View className="flex-1 bg-[#0B1220] items-center justify-center px-4">
-      <View className="w-full max-w-[360px] rounded-3xl border border-white/20 bg-white/10 p-5">
+    <View className="flex-1 bg-[#0B1220] items-center justify-start px-4 pt-10">
+      <View className="h-[80%] w-full max-w-[360px] rounded-3xl border border-white/20 bg-white/10 p-5">
         <Text className="text-white text-2xl font-bold text-center">
           RestaurantMenuScreen
         </Text>
         <Text className="mt-2 text-center text-white/70">category of menu</Text>
+        <SectionList
+          sections={menuSections}
+          className="m-6"
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Text className="mb-2 text-base text-white/90">{item}</Text>
+          )}
+          renderSectionHeader={({ section }) => (
+            <Text className="mb-3 mt-5 text-xl font-bold text-[#FACC15]">
+              {section.title}
+            </Text>
+          )}
+        />
       </View>
+      <Pressable
+        onPress={() => navigation.navigate("Sixth")}
+        className="mt-4 h-14 items-center justify-center rounded-2xl bg-[#FACC15]"
+      >
+        <Text className="text-base font-bold text-[#0B1220] px-4">
+          Move to next Project
+        </Text>
+      </Pressable>
     </View>
   );
 };
