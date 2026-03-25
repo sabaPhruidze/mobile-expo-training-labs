b6351ce88bd6e46a2a94585500785fcc507f3e19
Saba <sabaphruidze01@gmail.com>
2026-03-25 18:52:53 +0400

    feat(imageGalleryData): created data where imported random 5 image

---
+++ frontend/constants/imageGalleryData.ts
@@ -0,0 +1,31 @@
+export type GalleryImage = {
+  id: string;
+  imageUrl: string;
+};
+export const galleryImages: GalleryImage[] = [
+  {
+    id: "1",
+    imageUrl:
+      "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_hybrid&w=740&q=80",
+  },
+  {
+    id: "2",
+    imageUrl:
+      "https://www.shutterstock.com/image-photo/beauty-charisma-head-shot-portrait-600nw-2647728057.jpg",
+  },
+  {
+    id: "3",
+    imageUrl:
+      "https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=",
+  },
+  {
+    id: "4",
+    imageUrl:
+      "https://media.istockphoto.com/id/1682296067/photo/happy-studio-portrait-or-professional-man-real-estate-agent-or-asian-businessman-smile-for.jpg?s=612x612&w=0&k=20&c=9zbG2-9fl741fbTWw5fNgcEEe4ll-JegrGlQQ6m54rg=",
+  },
+  {
+    id: "5",
+    imageUrl:
+      "https://wallpapers.com/images/hd/cool-profile-picture-87h46gcobjl5e4xu.jpg",
+  },
+];
