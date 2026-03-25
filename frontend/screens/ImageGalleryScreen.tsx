b66bfd2257e44e2562e786032d1e29fbf478d6cd
Saba <sabaphruidze01@gmail.com>
2026-03-25 18:38:03 +0400

    feat(ImageGallery):now starting correcting seventh by turning it into image gallery

---
+++ frontend/screens/ImageGalleryScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ImageGalleryScreen = () => {
+  return (
+    <View>
+      <Text>ImageGalleryScreen</Text>
+    </View>
+  );
+};
+
+export default ImageGalleryScreen;
