cb4e3d14d679ef90658a4830fd7aa43a2572ab66
Saba <sabaphruidze01@gmail.com>
2026-03-25 18:15:51 +0400

    feat(RefreshTopicScreen): Added refreshing so on click it write 2 second refreshing yes and after 2 second no.

--- frontend/screens/RefreshTopicsScreen.tsx
+++ frontend/screens/RefreshTopicsScreen.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../Main";
 import { refreshTopicsData } from "../constants/refreshTopicData";
 type Props = NativeStackScreenProps<RootStackParamList, "RefreshTopicsScreen">;
 const RefreshTopicsScreen = ({ navigation }: Props) => {
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+
+  const handleRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  };
   return (
     <View className="flex-1 bg-slate-950 px-4 pt-6">
       <Text className="text-center text-2xl font-bold text-white">
@@ -15,6 +23,15 @@ const RefreshTopicsScreen = ({ navigation }: Props) => {
       </Text>
       <Text className="mt-6 text-center text-slate-400">
         Total Topics:{refreshTopicsData.length}
+      </Text>
+      <Text className="mt-4 text-center font-semibold text-yellow-500">
+        Refreshing: {refreshing ? "Yes" : "No"}
+      </Text>
+      <Text
+        onPress={handleRefresh}
+        className="mt-4 text-center font-semibold text-yellow-500"
+      >
+        Test refresh
       </Text>
     </View>
   );
